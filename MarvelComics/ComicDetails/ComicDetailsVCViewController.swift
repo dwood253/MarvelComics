9e51e2d54463112cf8636f5877023f4aa3b0a207
dwood <daniel.wood253@gmail.com>
2022-07-10 19:53:38 -0700

    layed out option buttons

--- MarvelComics/ComicDetails/ComicDetailsVCViewController.swift
+++ MarvelComics/ComicDetails/ComicDetailsVCViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 
 fileprivate let BUTTON_DIMENSION: CGFloat = 30
 fileprivate let BUTTON_PADDING: CGFloat = 10
+
 fileprivate let READ_NOW_TEXT = "READ NOW"
 fileprivate let MARK_AS_READ_ICON_IMAGE = "checkmark.circle.fill"
+fileprivate let READ_NOW_TEXT_HEIGHT: CGFloat = 40
+fileprivate let READ_NOW_TEXT_PADDING: CGFloat = 12
+
 fileprivate let MARK_AS_READ_TEXT = "MARK AS READ"
 fileprivate let ADD_TO_LIBRARY_ICON_IMAGE = "plus.circle.fill"
 fileprivate let ADD_TO_LIBRARY_TEXT = "ADD TO LIBRARY"
 fileprivate let READ_OFFLINE_ICON_IMAGE = "arrow.down.to.line.compact"
 fileprivate let READ_OFFLINE_TEXT = "READ OFFLINE"
 fileprivate let OPTION_BUTTON_ITEM_HEIGHT: CGFloat = 30
-fileprivate let OPTION_BUTTON_ITEM_PADDING: CGFloat = 8
+fileprivate let OPTION_BUTTON_ICON_DIMENSION: CGFloat = 15
+fileprivate let OPTION_BUTTON_ITEM_PADDING: CGFloat = 12
+fileprivate let OPTION_BUTTON_SEPERATOR_PADDING: CGFloat = 8
 fileprivate let COMIC_OPTIONS_SIDE_PADDING: CGFloat = 5
 
 class ComicDetailsVCViewController: UIViewController {
@@ -26,6 +32,7 @@ class ComicDetailsVCViewController: UIViewController {
         let v = UIView()
         v.translatesAutoresizingMaskIntoConstraints = false
         v.backgroundColor = Color.background
+        v.alpha = 0
         return v
     }()
     
@@ -69,6 +76,12 @@ class ComicDetailsVCViewController: UIViewController {
     let comicInfo: Comic
     let isPlaceholderImage: Bool
     var scrollViewTopAnchor: NSLayoutConstraint!
+    
+    lazy var buttonContainer: UIView = {
+        let v = UIView()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
     
     //MARK: - Lifecycle
     init(image: UIImage?, comicFrame: CGRect, comic: Comic, isPlaceholderImage: Bool) {
@@ -115,7 +128,7 @@ class ComicDetailsVCViewController: UIViewController {
     
     //MARK: - Setup UI Elements
     func setupUI() {
-        self.view.backgroundColor = Color.background
+        self.view.backgroundColor = Color.background?.withAlphaComponent(0)
         self.view.addSubviews([scrollView])
         scrollViewTopAnchor = scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor)
         NSLayoutConstraint.activate([
@@ -132,22 +145,49 @@ class ComicDetailsVCViewController: UIViewController {
             containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
             containerView.heightAnchor.constraint(equalToConstant: 900)
         ])
-        
-        
-        containerView.addSubviews([backgroundImage, comicImage, closeButton])
-        
+        setupButtonStack()
+        handlePlaceholder()
+        containerView.addSubviews([backgroundImage, comicImage, closeButton, buttonContainer])
         closeButton.addTarget(self, action: #selector(closeTapped), for: .touchUpInside)
-
         NSLayoutConstraint.activate([
             backgroundImage.widthAnchor.constraint(equalTo: containerView.widthAnchor),
             backgroundImage.heightAnchor.constraint(equalToConstant: self.view.frame.size.height/2),
             backgroundImage.topAnchor.constraint(equalTo: closeButton.bottomAnchor, constant: BUTTON_PADDING),
             backgroundImage.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            buttonContainer.leadingAnchor.constraint(equalTo: backgroundImage.leadingAnchor, constant: comicImage.frame.size.width + COMIC_OPTIONS_SIDE_PADDING*2),
+            buttonContainer.trailingAnchor.constraint(equalTo: backgroundImage.trailingAnchor, constant: -COMIC_OPTIONS_SIDE_PADDING),
+            buttonContainer.centerYAnchor.constraint(equalTo: backgroundImage.centerYAnchor),
             
             closeButton.topAnchor.constraint(equalTo: self.containerView.topAnchor, constant: BUTTON_PADDING),
             closeButton.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor, constant: -BUTTON_PADDING)
         ])
-
+    }
+    
+    func setupButtonStack() {
+        let readNowButton = getReadNowButton()
+        let markAsReadButton = getComicOptionsButton(iconName: MARK_AS_READ_ICON_IMAGE, text: MARK_AS_READ_TEXT, selector: #selector(markAsRead))
+        let addToLibraryButton = getComicOptionsButton(iconName: ADD_TO_LIBRARY_ICON_IMAGE, text: ADD_TO_LIBRARY_TEXT, selector: #selector(addToLibrary))
+        let readOfflineButton = getComicOptionsButton(iconName: READ_OFFLINE_ICON_IMAGE, text: READ_OFFLINE_TEXT, selector: #selector(readOffline))
+        
+        buttonContainer.addSubviews([readNowButton, markAsReadButton, addToLibraryButton, readOfflineButton])
+        NSLayoutConstraint.activate([
+            readNowButton.topAnchor.constraint(equalTo: buttonContainer.topAnchor),
+            readNowButton.leadingAnchor.constraint(equalTo: buttonContainer.leadingAnchor),
+            readNowButton.trailingAnchor.constraint(equalTo: buttonContainer.trailingAnchor),
+            markAsReadButton.topAnchor.constraint(equalTo: readNowButton.bottomAnchor, constant: OPTION_BUTTON_SEPERATOR_PADDING),
+            markAsReadButton.leadingAnchor.constraint(equalTo: readNowButton.leadingAnchor),
+            markAsReadButton.trailingAnchor.constraint(equalTo: readNowButton.trailingAnchor),
+            addToLibraryButton.topAnchor.constraint(equalTo: markAsReadButton.bottomAnchor, constant: OPTION_BUTTON_SEPERATOR_PADDING),
+            addToLibraryButton.leadingAnchor.constraint(equalTo: readNowButton.leadingAnchor),
+            addToLibraryButton.trailingAnchor.constraint(equalTo: readNowButton.trailingAnchor),
+            readOfflineButton.topAnchor.constraint(equalTo: addToLibraryButton.bottomAnchor, constant: OPTION_BUTTON_SEPERATOR_PADDING),
+            readOfflineButton.leadingAnchor.constraint(equalTo: readNowButton.leadingAnchor),
+            readOfflineButton.trailingAnchor.constraint(equalTo: readNowButton.trailingAnchor),
+            buttonContainer.bottomAnchor.constraint(equalTo: readOfflineButton.bottomAnchor, constant: OPTION_BUTTON_SEPERATOR_PADDING)
+        ])
+    }
+    
+    func handlePlaceholder() {
         if isPlaceholderImage {
             imageOverlayTitle.text = comicInfo.title ?? ""
             //add comic image title overlay
@@ -194,20 +234,29 @@ class ComicDetailsVCViewController: UIViewController {
     func getReadNowButton() -> UIView {
         let container = UIView()
         container.translatesAutoresizingMaskIntoConstraints = false
-        container.backgroundColor = UIColor(named: "read_now_button_color")
+        container.backgroundColor = Color.violet
         
         let textLabel = UILabel()
         textLabel.translatesAutoresizingMaskIntoConstraints = false
         textLabel.text = READ_NOW_TEXT
-        
-       
+        textLabel.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        textLabel.textColor = .lightGray
+        container.addSubview(textLabel)
+        NSLayoutConstraint.activate([
+            textLabel.topAnchor.constraint(equalTo: container.topAnchor, constant: READ_NOW_TEXT_PADDING),
+            textLabel.centerXAnchor.constraint(equalTo: container.centerXAnchor),
+            textLabel.heightAnchor.constraint(equalToConstant: READ_NOW_TEXT_HEIGHT),
+            textLabel.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -READ_NOW_TEXT_PADDING)
+        ])
+        container.isUserInteractionEnabled = true
+        container.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(readNow)))
         return container
     }
     
-    func getComicOptionsButton(iconName: String, text: String ) -> UIView {
+    func getComicOptionsButton(iconName: String, text: String, selector: Selector) -> UIView {
         let container = UIView()
         container.translatesAutoresizingMaskIntoConstraints = false
-        container.backgroundColor = .darkGray
+        container.backgroundColor = Color.background
         
         let iconImage = UIImageView(image: UIImage(systemName: iconName)?.withRenderingMode(.alwaysTemplate))
         iconImage.tintColor = .lightGray
@@ -222,25 +271,46 @@ class ComicDetailsVCViewController: UIViewController {
         textLabel.text = text
         textLabel.textColor = .lightGray
         textLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        
+        textLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
         container.addSubviews([iconImage, spacerPipe, textLabel])
         
         NSLayoutConstraint.activate([
             iconImage.topAnchor.constraint(equalTo: container.topAnchor, constant: OPTION_BUTTON_ITEM_PADDING),
-            iconImage.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: OPTION_BUTTON_ITEM_PADDING),
+            iconImage.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: OPTION_BUTTON_ITEM_PADDING/2),
             iconImage.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -OPTION_BUTTON_ITEM_PADDING),
+            iconImage.widthAnchor.constraint(equalToConstant: OPTION_BUTTON_ICON_DIMENSION),
+            iconImage.heightAnchor.constraint(equalToConstant: OPTION_BUTTON_ICON_DIMENSION),
             
-            spacerPipe.leadingAnchor.constraint(equalTo: iconImage.trailingAnchor, constant: OPTION_BUTTON_ITEM_PADDING),
-            spacerPipe.widthAnchor.constraint(equalToConstant: 2),
-            spacerPipe.heightAnchor.constraint(equalToConstant: OPTION_BUTTON_ITEM_HEIGHT),
+            spacerPipe.leadingAnchor.constraint(equalTo: iconImage.trailingAnchor, constant: OPTION_BUTTON_ITEM_PADDING/2),
+            spacerPipe.widthAnchor.constraint(equalToConstant: 1),
+            spacerPipe.heightAnchor.constraint(equalToConstant: OPTION_BUTTON_ITEM_HEIGHT - OPTION_BUTTON_SEPERATOR_PADDING),
             spacerPipe.centerYAnchor.constraint(equalTo: iconImage.centerYAnchor),
             
-            textLabel.leadingAnchor.constraint(equalTo: spacerPipe.trailingAnchor, constant: OPTION_BUTTON_ITEM_PADDING),
+            textLabel.leadingAnchor.constraint(equalTo: spacerPipe.trailingAnchor, constant: OPTION_BUTTON_ITEM_PADDING/2),
             textLabel.heightAnchor.constraint(equalToConstant: OPTION_BUTTON_ITEM_HEIGHT),
             textLabel.centerYAnchor.constraint(equalTo: iconImage.centerYAnchor)
         ])
+        container.isUserInteractionEnabled = true
+        let optionTappedGesture = UITapGestureRecognizer(target: self, action: selector)
+        container.addGestureRecognizer(optionTappedGesture)
         return container
+    }
+    
+    //MARK: - Comic Option Actions
+    @objc func readNow() {
+        print("readNow")
+    }
+    
+    @objc func markAsRead() {
+        print("markAsRead")
+    }
+    
+    @objc func addToLibrary() {
+        print("addToLibrary")
+    }
+    
+    @objc func readOffline() {
+        print("readOffline")
     }
     
 }
